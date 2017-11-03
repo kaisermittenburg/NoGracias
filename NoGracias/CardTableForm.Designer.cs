8fc9d4ab61d594b0135bc318dfb91327be7e721c
Andrew Growney <ajgrowney@ku.edu>
2017-11-03 10:06:56 -0500

    Card table work

--- NoGracias/CardTableForm.Designer.cs
+++ NoGracias/CardTableForm.Designer.cs
@@ -409,7 +409,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1628, 830);
+            this.ClientSize = new System.Drawing.Size(1815, 931);
             this.Controls.Add(this.pictureBox30);
             this.Controls.Add(this.pictureBox21);
             this.Controls.Add(this.pictureBox19);
