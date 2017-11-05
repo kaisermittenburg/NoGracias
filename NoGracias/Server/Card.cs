2e02f064cfb809b2d9694b1a14934e74d334c287
Juzer Zarif <juzerapj@gmail.com>
2017-11-05 17:58:48 -0600

    Added documentation

--- NoGracias/Server/Card.cs
+++ NoGracias/Server/Card.cs
@@ -13,13 +13,23 @@ namespace NoGracias.Server
         public int chipsOnCard { get; set; }
         #endregion
 
+        /**
+         * Default constructor with empty body
+         */
         public Card() { }
         
+        /**
+         * Overlaoded constructor setting card value
+         */
         public Card(int value)
         {
             this.value = value;
         }
 
+        /**
+         * Overloaded copy constructor makes a copy from parameter
+         * @param copy The source Card
+         */
         public Card(Card copy)
         {
             this.value = copy.value;
