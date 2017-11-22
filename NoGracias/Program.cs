a0730400e0826256f05ea0f62e89be8b490ee2bd
Juzer Zarif <juzerapj@gmail.com>
2017-11-22 13:35:43 -0600

    Minor scaling fix

--- NoGracias/Program.cs
+++ NoGracias/Program.cs
@@ -36,9 +36,9 @@ namespace NoGracias
             /////////////////////////////////////////////////////////////////////////////////////
             //   Remote Azure WCF Service Concept Prototype.
             //   Sends int, 2, to the service, service returns string, we print string to console
-            ConnectService.ConnectClient service = new ConnectService.ConnectClient();
-            Console.WriteLine(service.GetData(2));
-            Console.WriteLine(service.GetBent());
+            //ConnectService.ConnectClient service = new ConnectService.ConnectClient();
+            //Console.WriteLine(service.GetData(2));
+            //Console.WriteLine(service.GetBent());
             /////////////////////////////////////////////////////////////////////////////////////
 
             //Start Server Form
